54a7e801a25de8668e0f155b0320f12539bf1291
Andrey-Metelyov <andrey.metelyov@gmail.com>
2020-08-02 03:21:15 +0900

    done

--- Contacts (1)/task/src/contacts/Contact.java
+++ Contacts (1)/task/src/contacts/Contact.java
@@ -3,13 +3,16 @@ package contacts;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeParseException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
-public class Contact {
+public class Contact implements MenuHandler {
+    protected boolean person;
     private String name;
     private String number;
     private LocalDateTime timeCreated;
     private LocalDateTime timeLastEdit;
-    protected boolean person;
 
     Contact() {
         number = "[no number]";
@@ -22,29 +25,21 @@ public class Contact {
         setNumber(number);
     }
 
-    public boolean isPerson() {
-        return person;
-    }
-
-    public String getTimeCreated() {
-        return timeCreated.toString();
-    }
-
-    public String getTimeLastEdit() {
-        return timeLastEdit.toString();
-    }
-
-    public String getName() {
-        return name;
+    @Override
+    public String toString() {
+        return "Name: " + name + '\n' +
+                "Number: " + number + '\n' +
+                "Time created: " + timeCreated + '\n' +
+                "Time last edit: " + timeLastEdit + '\n';
     }
 
     protected void updateLastEditTime() {
         timeLastEdit = LocalDateTime.now().withSecond(0).withNano(0);
     }
 
-    public void setName(String name) {
-        updateLastEditTime();
-        this.name = name;
+    public static void main(String[] args) {
+        Contact c = new Person("1", "2", "+(with space)");
+        System.out.println(c.getNumber());
     }
 
     public String getNumber() {
@@ -62,7 +57,7 @@ public class Contact {
             }
             if (groups[1].matches("\\(?[\\d\\w]{2,}\\)?") &&
                     ((groups[0].contains("(") ^ groups[1].contains("(")) ||
-                    !groups[0].contains("(") && !groups[1].contains("("))) {
+                            !groups[0].contains("(") && !groups[1].contains("("))) {
                 for (int i = 2; i < groups.length; i++) {
                     if (!groups[i].matches("[\\d\\w]{2,}")) {
                         this.number = "[no number]";
@@ -76,9 +71,77 @@ public class Contact {
         this.number = "[no number]";
     }
 
-    public static void main(String[] args) {
-        Contact c = new Person("1", "2", "+(with space)");
-        System.out.println(c.getNumber());
+    public String getTimeCreated() {
+        return timeCreated.toString();
+    }
+
+    public String getTimeLastEdit() {
+        return timeLastEdit.toString();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        updateLastEditTime();
+        this.name = name;
+    }
+
+    @Override
+    public List<String> listFields() {
+        List<String> fields = new ArrayList<>();
+        fields.add("name");
+        fields.add("number");
+        return fields;
+    }
+
+    @Override
+    public void changeField(String field, String value) {
+        switch (field) {
+            case "name":
+                name = value;
+                break;
+            case "number":
+                number = value;
+                break;
+            default:
+                System.out.println("ERROR");
+        }
+    }
+
+    @Override
+    public String getField(String field) {
+        switch (field) {
+            case "name":
+                return name;
+            case "number":
+                return number;
+            default:
+                System.out.println("ERROR");
+                return "";
+        }
+    }
+
+    public void show() {
+        System.out.println(this.toString());
+    }
+
+    public void editMenu(Scanner scanner) {
+        System.out.print("Select a field (" + listFields().toString() + "): ");
+        String field = scanner.nextLine();
+        System.out.print("Enter " + field);
+        String value = scanner.nextLine();
+        changeField(field, value);
+    }
+
+    public boolean contains(String query) {
+        return name.toUpperCase().contains(query.toUpperCase()) ||
+                number.toUpperCase().contains(query.toUpperCase());
+    }
+
+    public String getDescName() {
+        return name;
     }
 }
 
@@ -95,6 +158,17 @@ class Person extends Contact {
     Person(String name, String surname, String number) {
         super(name, number);
         this.surname = surname;
+    }
+
+    @Override
+    public String toString() {
+        return "Name: " + getName() + '\n' +
+                "Surname: " + getSurname() + '\n' +
+                "Birth date: " + getBirthday() + '\n' +
+                "Gender: " + getGender() + '\n' +
+                "Number: " + getNumber() + '\n' +
+                "Time created: " + getTimeCreated() + '\n' +
+                "Time last edit: " + getTimeLastEdit() + '\n';
     }
 
     public String getSurname() {
@@ -138,6 +212,58 @@ class Person extends Contact {
             this.gender = "[no data]";
         }
     }
+
+    @Override
+    public List<String> listFields() {
+        List<String> fields = super.listFields();
+        fields.add("surname");
+        fields.add("birthday");
+        fields.add("gender");
+        return fields;
+    }
+
+    @Override
+    public void changeField(String field, String value) {
+        super.changeField(field, value);
+        switch (field) {
+            case "surname":
+                surname = value;
+                break;
+            case "birthday":
+                setBirthday(value);
+                break;
+            case "gender":
+                setGender(value);
+                break;
+            default:
+                System.out.println("ERROR");
+        }
+    }
+
+    @Override
+    public String getField(String field) {
+        String value = super.getField(field);
+        switch (field) {
+            case "surname":
+                return surname;
+            case "birthday":
+                return getBirthday();
+            case "gender":
+                return getGender();
+            default:
+                System.out.println("ERROR");
+        }
+        return value;
+    }
+
+    public boolean contains(String query) {
+        return super.contains(query) ||
+                surname.toUpperCase().contains(query.toUpperCase());
+    }
+
+    public String getDescName() {
+        return getName() + " " + getSurname();
+    }
 }
 
 class Organization extends Contact {
@@ -153,6 +279,14 @@ class Organization extends Contact {
         this.address = address;
     }
 
+    public String toString() {
+        return "Organization name: " + getName() + '\n' +
+                "Address: " + address + '\n' +
+                "Number: " + getNumber() + '\n' +
+                "Time created: " + getTimeCreated() + '\n' +
+                "Time last edit: " + getTimeLastEdit() + '\n';
+    }
+
     public String getAddress() {
         return address;
     }
@@ -160,5 +294,42 @@ class Organization extends Contact {
     public void setAddress(String address) {
         updateLastEditTime();
         this.address = address;
+    }
+    @Override
+    public List<String> listFields() {
+        List<String> fields = super.listFields();
+        fields.add("address");
+        return fields;
+    }
+
+    @Override
+    public void changeField(String field, String value) {
+        super.changeField(field, value);
+        switch (field) {
+            case "address":
+                address = value;
+                break;
+            default:
+                System.out.println("ERROR");
+        }
+    }
+
+    @Override
+    public String getField(String field) {
+        String value = super.getField(field);
+        switch (field) {
+            case "address":
+                return address;
+        }
+        return value;
+    }
+
+    public boolean contains(String query) {
+        return super.contains(query) ||
+                address.toUpperCase().contains(query.toUpperCase());
+    }
+
+    public String getDescName() {
+        return getName();
     }
 }
--- Contacts (1)/task/src/contacts/Main.java
+++ Contacts (1)/task/src/contacts/Main.java
@@ -1,35 +1,48 @@
 package contacts;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     static Scanner scanner = new Scanner(System.in);
-    static List<Contact> contacts = new ArrayList<>();
+    static Phonebook phonebook;
+//    static List<Contact> contacts = new ArrayList<>();
+
     public static void main(String[] args) {
+        phonebook = new Phonebook();
+        if (args.length > 0) {
+            String filename = args[0];
+            System.out.println("open " + filename);
+            phonebook.deserialize(filename);
+        }
         menu();
     }
 
     private static void menu() {
         while (true) {
-            System.out.println("\nEnter action (add, remove, edit, count, list, exit):");
+            System.out.print("\n[menu] Enter action (add, list, search, count, exit): ");
             String choice = scanner.nextLine();
             switch (choice) {
                 case "add":
                     add();
                     break;
-                case "remove":
-                    remove();
+                case "list":
+                    list();
                     break;
-                case "edit":
-                    edit();
+                case "search":
+                    search();
                     break;
                 case "count":
                     count();
                     break;
                 case "info":
-                    info();
+//                    info();
+                    break;
+                case "remove":
+//                    remove();
+                    break;
+                case "edit":
+//                    edit();
                     break;
                 case "exit":
                     return;
@@ -37,18 +50,57 @@ public class Main {
         }
     }
 
-    private static void list() {
+    private static void search() {
+        System.out.print("Enter search query: ");
+        String query = scanner.nextLine();
+        List<Contact> search = phonebook.search(query);
+        showContacts(search);
+        System.out.print("\n[search] Enter action ([number], back, again): ");
+        String action = scanner.nextLine();
+        switch (action) {
+            case "back":
+                return;
+            case "again":
+                break;
+        }
+        int index = Integer.parseInt(action);
+        Contact contact = search.get(index - 1);
+        contact.show();
+        showRecordMenu(contact);
+    }
+
+    private static void showContacts(List<Contact> contacts) {
         int counter = 1;
+
         for (Contact contact : contacts) {
-            System.out.println(counter++ + ". " + contact.getName());
+            System.out.println(counter++ + ". " + contact.getDescName());
         }
     }
 
-    private static void info() {
-        list();
-        System.out.println("Enter index to show info:");
-        int index = Integer.parseInt(scanner.nextLine());
-        showContact(contacts.get(index - 1));
+    private static void list() {
+        showContacts(phonebook.getContacts());
+        System.out.print("\n[list] Enter action ([number], back): ");
+        String action = scanner.nextLine();
+        if ("back".equals(action)) {
+            return;
+        }
+        int index = Integer.parseInt(action);
+        Contact contact = phonebook.get(index - 1);
+        contact.show();
+        showRecordMenu(contact);
+    }
+
+    private static void showRecordMenu(Contact contact) {
+        System.out.print("[record] Enter action (edit, delete, menu): ");
+        String action = scanner.nextLine();
+        switch (action) {
+            case "edit":
+                contact.editMenu(scanner);
+                break;
+            case "delete":
+            case "menu":
+                break;
+        }
     }
 
     private static void showContact(Contact contact) {
@@ -85,7 +137,7 @@ public class Main {
             person.setGender(scanner.nextLine());
             System.out.print("Enter the number: ");
             person.setNumber(scanner.nextLine());
-            contacts.add(person);
+            phonebook.add(person);
         } else if ("organization".equals(type)) {
             contact = new Organization();
             Organization organization = (Organization) contact;
@@ -95,84 +147,84 @@ public class Main {
             organization.setAddress(scanner.nextLine());
             System.out.print("Enter the number: ");
             organization.setNumber(scanner.nextLine());
-            contacts.add(organization);
+            phonebook.add(organization);
         }
         System.out.println("The record added.");
     }
 
     private static void remove() {
-        if (contacts.isEmpty()) {
+        if (phonebook.count() == 0) {
             System.out.println("No records to remove!");
         } else {
-            info();
+            showContacts(phonebook.getContacts());
             System.out.print("Select a record: ");
             int number = Integer.parseInt(scanner.nextLine());
-            contacts.remove(number - 1);
+            phonebook.remove(number - 1);
             System.out.println("The record removed!");
         }
     }
 
-    private static void edit() {
-        if (contacts.isEmpty()) {
-            System.out.println("No records to edit!");
-        } else {
-            list();
-            System.out.print("Select a record: ");
-            int number = Integer.parseInt(scanner.nextLine());
-            Contact contact = contacts.get(number - 1);
-            if (contact.isPerson()) {
-                Person person = (Person) contact;
-                System.out.print("Select a field (name, surname, birth, gender, number): ");
-                String field = scanner.nextLine();
-                switch (field) {
-                    case "name":
-                        System.out.print("Enter name: ");
-                        person.setName(scanner.nextLine());
-                        break;
-                    case "surname":
-                        System.out.print("Enter surname: ");
-                        person.setSurname(scanner.nextLine());
-                        break;
-                    case "birth":
-                        System.out.print("Enter birthdate: ");
-                        person.setBirthday(scanner.nextLine());
-                        break;
-                    case "gender":
-                        System.out.print("Enter gender: ");
-                        person.setGender(scanner.nextLine());
-                        break;
-                    case "number":
-                        System.out.print("Enter number: ");
-                        person.setNumber(scanner.nextLine());
-                        break;
-                }
-            } else {
-                Organization organization = (Organization) contact;
-                System.out.print("Select a field (name, address, number): ");
-                String field = scanner.nextLine();
-                switch (field) {
-                    case "name":
-                        System.out.print("Enter name: ");
-                        organization.setName(scanner.nextLine());
-                        break;
-                    case "address":
-                        System.out.print("Enter address: ");
-                        organization.setAddress(scanner.nextLine());
-                        break;
-                    case "number":
-                        System.out.print("Enter number: ");
-                        organization.setNumber(scanner.nextLine());
-                        break;
-                    default:
-                        System.out.println("ERROR");
-                }
-            }
-            contacts.set(number - 1, contact);
-            System.out.println("The record updated!");
-        }
-    }
+//    private static void edit() {
+//        if (phonebook.count() == 0) {
+//            System.out.println("No records to edit!");
+//        } else {
+//            list();
+//            System.out.print("Select a record: ");
+//            int number = Integer.parseInt(scanner.nextLine());
+//            Contact contact = phonebook.get(number - 1);
+//            if (contact.isPerson()) {
+//                Person person = (Person) contact;
+//                System.out.print("Select a field (name, surname, birth, gender, number): ");
+//                String field = scanner.nextLine();
+//                switch (field) {
+//                    case "name":
+//                        System.out.print("Enter name: ");
+//                        person.setName(scanner.nextLine());
+//                        break;
+//                    case "surname":
+//                        System.out.print("Enter surname: ");
+//                        person.setSurname(scanner.nextLine());
+//                        break;
+//                    case "birth":
+//                        System.out.print("Enter birthdate: ");
+//                        person.setBirthday(scanner.nextLine());
+//                        break;
+//                    case "gender":
+//                        System.out.print("Enter gender: ");
+//                        person.setGender(scanner.nextLine());
+//                        break;
+//                    case "number":
+//                        System.out.print("Enter number: ");
+//                        person.setNumber(scanner.nextLine());
+//                        break;
+//                }
+//            } else {
+//                Organization organization = (Organization) contact;
+//                System.out.print("Select a field (name, address, number): ");
+//                String field = scanner.nextLine();
+//                switch (field) {
+//                    case "name":
+//                        System.out.print("Enter name: ");
+//                        organization.setName(scanner.nextLine());
+//                        break;
+//                    case "address":
+//                        System.out.print("Enter address: ");
+//                        organization.setAddress(scanner.nextLine());
+//                        break;
+//                    case "number":
+//                        System.out.print("Enter number: ");
+//                        organization.setNumber(scanner.nextLine());
+//                        break;
+//                    default:
+//                        System.out.println("ERROR");
+//                }
+//            }
+//            phonebook.set(number - 1, contact);
+//            System.out.println("The record updated!");
+//        }
+//    }
 
     private static void count() {
-        System.out.println("The Phone Book has " + contacts.size() + " records.");
+        System.out.println("The Phone Book has " + phonebook.count() + " records.");
     }
 }
